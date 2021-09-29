73aa38b289375d9ac3ef3fdb02a66f49ca6fbca2
Crowdbotics <team@crowdbotics.com>
2021-09-29 04:09:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "raspy_fog_30951",
+  url: "https://raspy_fog_30951.botics.co",
+  api: "https://raspy_fog_30951.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
